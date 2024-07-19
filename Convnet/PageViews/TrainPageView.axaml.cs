e7260cda21ec53d26956bafcd90c094728b0e695
uapconvnet <dhaenefilip99@gmail.com>
2024-07-20 01:01:49 +0200

    latest build

--- Convnet/PageViews/TrainPageView.axaml.cs
+++ Convnet/PageViews/TrainPageView.axaml.cs
@@ -3,7 +3,7 @@ using Avalonia.Markup.Xaml;
 
 namespace Convnet.PageViews
 {
-    private bool Zoomout = false;
+    // private bool Zoomout = false;
 
     public partial class TrainPageView : UserControl
     {
