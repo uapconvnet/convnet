4c243b7028fb6d57a96a67d26164adbb267bc8a6
uapconvnet <dhaenefilip99@gmail.com>
2024-07-30 07:32:24 +0200

    latest build

--- dnn/build.cmd
+++ dnn/build.cmd
@@ -1,6 +1,6 @@
 mkdir build
 cd build
-cmake -A x64 .. -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON
+cmake -A x64 .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON
 msbuild dnn.sln /p:Configuration=Debug
 msbuild dnn.sln /p:Configuration=Release
 cd ..
