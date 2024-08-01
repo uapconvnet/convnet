d386869749141e7b2caeacc6a5a7ac2460250643
uapconvnet <dhaenefilip99@gmail.com>
2024-08-01 05:50:48 +0200

    latest build

--- build.sh
+++ build.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 cd dnn
 chmod +rwx ./build.sh
 ./build.sh
 cd ..
 cd Convnet
 dotnet build Convnet.csproj -c:Release
+cd ..
+
+echo "[Desktop Entry]
+Encoding=UTF-8
+Version=1.0
+Type=Application
+Terminal=false
+Exec=${SCRIPT_DIR}/Convnet/bin/Release/net8.0/Convnet
+Name=Convnet
+Icon=${SCRIPT_DIR}/Convnet/Resources/App.ico
+Comment=
+NoDisplay=false" > ${HOME}/.local/share/applications/convnet.desktop
