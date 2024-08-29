1b937c11bacaa2174bdcaebb9dfaac469dea3227
uapconvnet <dhaenefilip99@gmail.com>
2024-08-29 14:07:36 +0200

    latest build

--- dnn/include/Dataprovider.h
+++ dnn/include/Dataprovider.h
@@ -94,6 +94,7 @@ namespace dnn
 				if (std::filesystem::exists(path / "wnids.txt") && std::filesystem::exists(path / "words.txt"))
 					available = true;
 				break;
+
 			default:
 				available = false;
 			}
