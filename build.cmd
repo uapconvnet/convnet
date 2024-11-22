819df7434b9edc1738a420b87cc363f673e07374
uapconvnet <dhaenefilip99@gmail.com>
2024-11-22 18:44:44 +0100

    latest build

--- build.cmd
+++ build.cmd
@@ -1,5 +1,8 @@
+ECHO OFF
+SET mode=%1
+IF mode EQU "" SET mode="Release"
 cd dnn
 call build.cmd %1
 cd ..
 dotnet restore
-msbuild Convnet.sln /p:Configuration=%1
+msbuild Convnet.sln /p:Configuration=%mode%
--- dnn/build.cmd
+++ dnn/build.cmd
@@ -1,5 +1,8 @@
+ECHO OFF
+SET mode=%1
+IF mode EQU "" SET mode="Release"
 mkdir build
 cd build
-cmake -A x64 .. -DCMAKE_BUILD_TYPE=%1 -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON
-msbuild dnn.sln /p:Configuration=%1
+cmake -A x64 .. -DCMAKE_BUILD_TYPE=%mode% -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON
+msbuild dnn.sln /p:Configuration=%mode%
 cd ..
