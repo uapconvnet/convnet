3f70937b6a4d1acdf976503a61a93532b5873d05
uapconvnet <dhaenefilip99@gmail.com>
2026-07-01 23:08:06 +0200

    latest build

--- Convnet/PageViews/PageView.axaml.cs
+++ Convnet/PageViews/PageView.axaml.cs
@@ -1,7 +1,8 @@
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
 using Convnet.PageViewModels;
 using ReactiveUI.Avalonia;
-
 namespace Convnet.PageViews
 {
     public partial class PageView : ReactiveUserControl<PageViewModel>
@@ -14,6 +15,11 @@ namespace Convnet.PageViews
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void CmdToolBar_GotFocus(object? sender, Avalonia.Input.FocusChangedEventArgs e)
+        {
+            Dispatcher.UIThread.Post(() => e.OldFocusedElement?.Focus());
         }
     }
 }
