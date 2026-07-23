8ecdf23ca4c656a91aabddc1aa6e202e2d6658d2
uapconvnet <dhaenefilip99@gmail.com>
2026-07-23 08:32:56 +0200

    latest build

--- Convnet/PageViews/TestPageView.axaml.cs
+++ Convnet/PageViews/TestPageView.axaml.cs
@@ -1,4 +1,6 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using System.Data;
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Convnet.PageViewModels;
 using ReactiveUI.Avalonia;
@@ -15,11 +17,35 @@ namespace Convnet.PageViews
 
             InitializeComponent();
 
-            //var grid = this.FindControl<DataGrid>("Datagrid");
-            //if (grid != null)
-            //{
+            var datagrid = this.FindControl<DataGrid>("Datagrid");
+            if (datagrid != null)
+            {
+                datagrid.AutoGeneratingColumn += Datagrid_AutoGeneratingColumn;
+                datagrid.DataContextChanged += Datagrid_DataContextChanged;
+            }
+        }
 
-            //}
+        private void Datagrid_AutoGeneratingColumn(object? sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            e.Cancel = (e.PropertyName == "DataView" || e.PropertyName == "Item" || e.PropertyName == "RowVersion" || e.PropertyName == "Row" || e.PropertyName == "IsNew" || e.PropertyName == "IsEdit");
+        }
+            
+        private void Datagrid_DataContextChanged(object? sender, System.EventArgs e)
+        {
+            var datagrid = sender as DataGrid;
+            if (datagrid != null)
+            {
+                var tpvm = DataContext as TestPageViewModel;
+                if (tpvm != null && tpvm.ConfusionDataTable != null)
+                {
+                    while (datagrid.Columns.Count > 0) { datagrid.Columns.RemoveAt(datagrid.Columns.Count - 1); }
+
+                    datagrid.ItemsSource = tpvm.ConfusionDataTable.DefaultView;
+
+                    foreach (System.Data.DataColumn x in tpvm.ConfusionDataTable.Columns)
+                        datagrid.Columns.Add(new DataGridTextColumn { Header = x.ColumnName, Binding = new Avalonia.Data.Binding($"Row.ItemArray[{x.Ordinal}]") });
+                }
+            }
         }
 
         private void InitializeComponent()
